378ce2b32e324ac8e5f402e17d7df74799f7c22b
Vitaly Gorelik <vitalygorelik1@gmail.com>
2020-12-13 19:04:55 +0200

    WIP

--- lets-do-it-client/lets-do-it-client/src/app/app.module.ts
+++ lets-do-it-client/lets-do-it-client/src/app/app.module.ts
@@ -5,16 +5,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { HeaderComponent } from './header/header.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     CommonModule,
-    MatButtonModule
+    MatButtonModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
